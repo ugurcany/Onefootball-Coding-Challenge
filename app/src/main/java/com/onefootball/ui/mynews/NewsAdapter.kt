0c88fa21e5b13d5bae60e5a48ac4fdd810784095
Ugurcan Yildirim <ugurcany@outlook.com>
2019-11-14 20:35:42 +0300

    Refactor news adapter

--- app/src/main/java/com/onefootball/ui/mynews/NewsAdapter.kt
+++ app/src/main/java/com/onefootball/ui/mynews/NewsAdapter.kt
@@ -3,22 +3,23 @@ package com.onefootball.ui.mynews
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.onefootball.R
+import com.onefootball.databinding.ViewNewsItemBinding
 import com.onefootball.model.News
 
-class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter : RecyclerView.Adapter<NewsViewHolder>() {
 
     private val newsItems = ArrayList<News>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_news_item, parent, false)
-        return NewsViewHolder(view)
+        val binding = DataBindingUtil.inflate<ViewNewsItemBinding>(
+            LayoutInflater.from(parent.context), R.layout.view_news_item, parent, false
+        )
+        return NewsViewHolder(binding)
     }
 
     override fun getItemCount() = newsItems.size
@@ -26,14 +27,17 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val news = newsItems[position]
 
-        holder.titleView.text = news.title
-        holder.newsView.load(url = news.imageURL)
-        holder.resourceImage.load(url = news.resourceURL)
-        holder.resourceName.text = news.resourceName
-        holder.itemView.setOnClickListener {
-            it.context.startActivity(
-                Intent(Intent.ACTION_VIEW, Uri.parse(news.newsLink))
-            )
+        holder.binding.apply {
+            newsTitle.text = news.title
+            newsView.load(url = news.imageURL)
+            resourceIcon.load(url = news.resourceURL)
+            resourceName.text = news.resourceName
+
+            root.setOnClickListener {
+                it.context.startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(news.newsLink))
+                )
+            }
         }
     }
 
@@ -43,11 +47,6 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
         notifyDataSetChanged()
     }
 
-    class NewsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-        var titleView: TextView = itemView.findViewById(R.id.news_title)
-        var newsView: ImageView = itemView.findViewById(R.id.news_view)
-        var resourceImage: ImageView = itemView.findViewById(R.id.resource_icon)
-        var resourceName: TextView = itemView.findViewById(R.id.resource_name)
-    }
 }
+
+class NewsViewHolder(var binding: ViewNewsItemBinding) : RecyclerView.ViewHolder(binding.root)
